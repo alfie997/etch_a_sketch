5b93482ec1fc938e8267b4fd172d3f6e2b098187
alfie997 <161500593+alfie997@users.noreply.github.com>
2024-06-17 15:37:59 +0300

    Implement sketch action
    main.js
     - Add each square element to an array
     - Make the square grey if the mouse hovers over it
     - Test (works) but no refactoring yet

--- main.js
+++ main.js
@@ -1,18 +1,46 @@
 const container = document.querySelector(".container");
 
-// const elements = [];
+const squares = [];
+let index = 0;
 
+// const elements = [];
 // let box = 1;
 
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const square = document.createElement("div");
         square.classList.add("square");
+        container.appendChild(square);
+        squares[index] = square;
+        index++;
+
         // square.textContent = box;
         // box++;
-        container.appendChild(square);
-        // elements[i*j] = square;
+        // elements[(i+1)*(j+1)] = square;
     }
 }
 
+console.log(squares[0]);
+console.log(squares.length);
+
+container.addEventListener('mouseenter', (e) => {
+    for (const square of squares) {
+        if (square != undefined) {
+            square.addEventListener('mousemove', (e) => {
+                square.style.cssText = "background-color: grey;";
+            });
+        }
+    }
+});
+
 // console.log(elements.length);
+
+// const square = document.querySelector(".square");
+
+// square.addEventListener("mousemove", () => {
+//     square.style.cssText = "background-color: grey;"
+// });
+
+// function colorSquare(element) {
+//     element.style.cssText = "background-color: grey;"
+// }
