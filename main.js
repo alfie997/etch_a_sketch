a10c41fe21b05f364c4f4cb7c0df5a9ec71107db
alfie997 <161500593+alfie997@users.noreply.github.com>
2024-06-17 17:03:56 +0300

    Implement resizer 'unfinished'
    index.html
     - Add button element for resizing
    
    styles.css
     - Make .square height and width properties variable
    
    main.js
     - Make number of squares variable
     - Add event for resizing button
     - Implement early logic for resizing (doesn't work)

--- main.js
+++ main.js
@@ -1,21 +1,57 @@
 const container = document.querySelector(".container");
+const resizer = document.querySelector(".resizer");
 
 const squares = [];
-let index = 0;
+
+let input = 16;
+
+let height = 500 / input;
+let width = 500 / input;
+
+const r = document.querySelector(":root");
+r.style.setProperty("--square-height", `${height}px`);
+r.style.setProperty("--square-width", `${width}px`);
+
+resizer.addEventListener("click", (e) => {
+    let newInput = parseInt(prompt());
+    // if (newInput < 100) {
+    //     input = newInput;
+    // }
+    height = 500 / input;
+    width = 500 / input;
+    r.style.setProperty("--square-height", `${height}px`);
+    r.style.setProperty("--square-width", `${width}px`);
+    
+    if (input < newInput) {
+        input = newInput;
+    } else if (input > newInput) {
+        input = input - newInput;
+    }
+});
+
+// const rs = getComputedStyle(r);
+// rs.getPropertyValue("--square-height") = height;
+// rs.getPropertyValue("--square-width") = width;
+
+// console.log(height);
 
 // const elements = [];
-// let box = 1;
+let box = 1;
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
+let index = 0;
+
+for (let i = 0; i < input; i++) {
+    for (let j = 0; j < input; j++) {
         const square = document.createElement("div");
         square.classList.add("square");
         container.appendChild(square);
         squares[index] = square;
         index++;
 
-        // square.textContent = box;
-        // box++;
+        // square.style.cssText = `height: ${height}; width: ${width};`;
+        // square.style.cssText = "height: 32px; width: 32px;";
+        square.textContent = box;
+        box++;
         // elements[(i+1)*(j+1)] = square;
     }
 }
@@ -23,10 +59,10 @@ for (let i = 0; i < 16; i++) {
 console.log(squares[0]);
 console.log(squares.length);
 
-container.addEventListener('mouseenter', (e) => {
+container.addEventListener("mouseenter", (e) => {
     for (const square of squares) {
         if (square != undefined) {
-            square.addEventListener('mousemove', (e) => {
+            square.addEventListener("mousemove", (e) => {
                 square.style.cssText = "background-color: grey;";
             });
         }
